4ac3f39d096924df0e560d7e150542e64dd04141
Joel Angell <j.angell07@gmail.com>
2018-02-28 23:13:37 -0800

    Add audio element

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,15 +1,11 @@
-
-
-var gifArray = ["Burt Maclin", "Ron Swanson", "Dennis Feinsten"]
+var gifArray = ["Ron Swanson", "Leslie Knope", "April Ludgate", "Tom Haverford", "Ben Wyatt", "Jerry Gergich", "Donna Meagle", "Craig Middlebrooks", "Jean Ralphio", "Councilman Jamm", "Lil Sebastian", "Burt Macklin", "Janet Snakehole", "Mona Lisa Saperstein", "Johnny Karate", "DJ Roomba", "Chris Traeger", "Ann Perkins"]
 var newGif
-
-
 
 
 function createButtons() {
     $("#button-row").empty()
     for (i = 0; i < gifArray.length; i++) {
-        $("#button-row").append("<button class = 'gif-buttons' id = 'gif-button-" + [i] + "' value = '" + gifArray[i] + "'>" + gifArray[i] + "</button>")
+        $("#button-row").append("<button class = 'btn btn-success gif-buttons' id = 'gif-button-" + [i] + "' value = '" + gifArray[i] + "'>" + gifArray[i] + "</button>")
     }
 }
 createButtons()
@@ -18,27 +14,39 @@ $("#searchButton").on("click", function () {
     var newGif = $("#gif-input").val()
     gifArray.push(newGif)
     createButtons()    
+    $("#gif-input").val('')
 })
 
-$(".gif-buttons").on("click", function() {
+$("#button-row").on("click", ".gif-buttons", function() {
     var gifSearch = $(this).val()
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&rating=g&lang=en&api_key=JTlkrJVw0NOuhHwqFjcH5kgpUov3CF42&limit=10"
+    console.log($(this).val())
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "-parks-and-recreation&lang=en&api_key=JTlkrJVw0NOuhHwqFjcH5kgpUov3CF42&limit=12"
     $("#gif-row").empty()
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         for (i = 0; i < response.data.length; i++) {
-            console.log(response.data[i].images.original.original_still.url)
-            $("#gif-row").append("<div class='col-sm-6 col-md-4'><a class= 'lightbox'><img src= '" + response.data[i].images.original_still.url + "' data-still: '" + response.data[i].images.original_still.url + "' data-animate: '" + response.data[i].images.original.url + "' id = 'img-" + [i] + "'></a></div>")
+            console.log(response.data[i])
+            var animated = response.data[i].images.original.url
+            var still = response.data[i].images.original_still.url
+            var rating = response.data[i].rating.toUpperCase()
+            $("#gif-row").append('<div class= "col-sm-6 col-md-4"><a class= "lightbox"><img src= "' + still + '" data-animate= "' + animated + '" data-still= "' + still + '" data-state = "still" class = "gif-class" id = "img-' + [i] + '"></a><p>Rating: ' + rating + '</div>')
         }
     });
 })
 
-var count = 0
-$("#gif-row").on("click", function() {
-    console.log(this)
-  count++
-  if(count%2) {$(this).attr("src", $(this).attr("data-animate"))}
-  else { $(this).attr("src", $(this).attr("data-still"))}
+
+
+
+$("#gif-row").on("click", 'img', function() {
+    if ($(this).attr("data-state") === "still") {
+      $(this).attr("src", $(this).attr("data-animate"))
+      $(this).attr("data-state", "animate") 
+    }
+
+    else {
+      $(this).attr("src", $(this).attr("data-still"))
+      $(this).attr("data-state", "still")
+    }
 })
